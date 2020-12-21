6d91cfcc823c30d21c3bc1cbd17500a96400c39a
chenhui <1020598520@qq.com>
2020-12-21 15:27:39 +0800

    1.add the homepage 2. set  bottomNav

---
+++ lib/home/home_page.dart
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: chen hui
+ * @Date: 2020-12-21 14:29:56
+ */
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
